9d422965968ceba1495c8a584da041dbd2580cfe
Swapnil Joshi <E010459@cigniti.com>
2024-12-19 11:51:36 +0530

    added testcases

--- tests/rsg.ps1
+++ tests/rsg.ps1
@@ -5,6 +5,9 @@ Describe "Check if Resource Group exists" {
     $resourceGroupName = "NetworkWatcherRG"
 
     It "should exist in Azure" {
+        # Debugging: Print the value of $resourceGroupName
+        Write-Host "Resource Group Name: $resourceGroupName"
+
         # Use Get-AzResourceGroup to check if the resource group exists
         $resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
         
