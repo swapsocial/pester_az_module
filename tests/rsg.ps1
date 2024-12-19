61dc12d630ee387120898555b1f7507b88c55874
Swapnil Joshi <E010459@cigniti.com>
2024-12-19 12:08:10 +0530

    added testcases

--- tests/rsg.ps1
+++ tests/rsg.ps1
@@ -1,17 +1,30 @@
-Import-Module -Name Az
+Import-Module -Name Az.Resources
+$subscriptionId = "8dd8b7ef-db34-43f4-bbce-997ece61cb8d"
+Select-AzSubscription -Subscription $subscriptionId
+# Log environment variables
+Write-Output "-----------------------------------------------------------------------"
+Write-Output " "
+Describe 'Azure VM Tests' {
 
-Describe "Check if Resource Group exists" {
-
-    $resourceGroupName = "NetworkWatcherRG"
-
-    It "should exist in Azure" {
-        # Debugging: Print the value of $resourceGroupName
-        Write-Host "Resource Group Name: $resourceGroupName"
-
-        # Use Get-AzResourceGroup to check if the resource group exists
-        $resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
-        
-        # Assert that the resource group was found
-        $resourceGroup | Should -Not -BeNullOrEmpty
+    # Test 1: VM should exist
+    It 'VM should exist' {
+        $vm = Get-AzVM -ResourceGroupName 'YourResourceGroupName' -Name 'YourVMName'
+        $vm | Should -Not -BeNullOrEmpty
     }
+
+    # Test 2: VM should be in running state
+    It 'VM should be in running state' {
+        $vm = Get-AzVM -ResourceGroupName 'YourResourceGroupName' -Name 'YourVMName'
+        $vm.Statuses[1].Code | Should -Be 'PowerState/running'
+    }
+
+    # Test 3: VM should have correct size
+    It 'VM should have correct size' {
+        $vm = Get-AzVM -ResourceGroupName 'YourResourceGroupName' -Name 'YourVMName'
+        $vm.HardwareProfile.VmSize | Should -Be 'Standard_DS1_v2'  # Change this to your expected size
+    }
+
 }
+# Log environment variables
+Write-Output "------------------------------------------------------------------------"
+Write-Output " "
