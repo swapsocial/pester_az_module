12bf407824b47280ffeb06561b7acc0d2dc6164e
Swapnil Joshi <E010459@cigniti.com>
2024-12-19 12:23:53 +0530

    added testcases

--- tests/rsg.ps1
+++ tests/rsg.ps1
@@ -1,9 +1,8 @@
 Import-Module -Name Az.Resources
+
 $subscriptionId = "0b7e0b83-0035-48fe-92fe-bf1143f2e715"
 Select-AzSubscription -Subscription $subscriptionId
-# Log environment variables
-Write-Output "-----------------------------------------------------------------------"
-Write-Output " "
+
 Describe 'Azure VM Tests' {
 
     # Test 1: VM should exist
@@ -12,22 +11,18 @@ Describe 'Azure VM Tests' {
         $vm | Should -Not -BeNullOrEmpty
     }
 
-# Test 2: VM should be in running state
+    # Test 2: VM should be in running state
     It 'VM should be in running state' {
         # Get VM details
         $vm = Get-AzVM -ResourceGroupName 'YourResourceGroupName' -Name 'YourVMName'
         $status = $vm.Statuses | Where-Object { $_.Code -eq 'PowerState/running' }
         $status | Should -Not -BeNullOrEmpty
     }
-}
 
     # Test 3: VM should have correct size
     It 'VM should have correct size' {
         $vm = Get-AzVM -ResourceGroupName 'YourResourceGroupName' -Name 'YourVMName'
-        $vm.HardwareProfile.VmSize | Should -Be 'Standard_B1s'  # Change this to your expected size
+        $vm.HardwareProfile.VmSize | Should -Be 'Standard_B1s'  
     }
 
 }
-# Log environment variables
-Write-Output "------------------------------------------------------------------------"
-Write-Output " "
